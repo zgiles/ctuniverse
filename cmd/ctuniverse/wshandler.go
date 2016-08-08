ce54278d02c2379c726ca6a9ec72f230f9abe5d2
Zachary Giles <zgiles@gmail.com>
2016-08-07 22:02:21 -0400

    redoing types etc.. nto working yet

--- cmd/ctuniverse/wshandler.go
+++ cmd/ctuniverse/wshandler.go
@@ -11,13 +11,19 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+type client struct {
+	uuid string
+	attributes map[string]string
+}
+
 func wshandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("Subscribed: ")
+	c := client{}
+	fmt.Println("New Client")
 	for {
 		_ = conn.WriteMessage(websocket.TextMessage, []byte("{ \"uuid\": \"123-212-321-231\" }"))
 		_, msg, err := conn.ReadMessage()
