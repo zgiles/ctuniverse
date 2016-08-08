1be92a222e8cff72a31e63a9ceadd7d6251274f3
Zachary Giles <zgiles@gmail.com>
2016-08-07 20:51:01 -0400

    Changing to attributes style

--- universeobject.go
+++ universeobject.go
@@ -6,9 +6,13 @@ type UniverseObject struct {
 	Type string `json:"type" redis:"type"`
 	Global_X int64 `json:"global_x" redis:"global_x"`
 	Global_Y int64 `json:"global_y" redis:"global_y"`
-	Fuel int64 `json:"fuel" redis:"fuel"`
-	Weight int64 `json:"fuel" redis:"weight"`
-	Decay int64 `json:"decay" redis:"decay"`
 	Velocity_x int64 `json:"velocity_x" redis:"velocity_x"`
 	Velocity_y int64 `json:"velocity_y" redis:"velocity_y"`
+	Attributes map[string]string `json:"attributes" redis:"attributes"`
 }
+
+
+// These will be in attributes for now
+// Decay int64 `json:"decay" redis:"decay"`
+// Fuel int64 `json:"fuel" redis:"fuel"`
+// Weight int64 `json:"weight" redis:"weight"`
