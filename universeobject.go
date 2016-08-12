7443d70cfbed9a7843be107ddfcfaf1600d20dc9
Zachary Giles <zgiles@gmail.com>
2016-08-11 23:34:01 -0400

    go fmt

--- universeobject.go
+++ universeobject.go
@@ -1,16 +1,15 @@
 package ctuniverse
 
 type UniverseObject struct {
-	Uuid string `json:"uuid" redis:"uuid"`
-	Owner string `json:"owner" redis:"owner"`
-	Type string `json:"type" redis:"type"`
-	Global_X int64 `json:"global_x" redis:"global_x"`
-	Global_Y int64 `json:"global_y" redis:"global_y"`
-	Velocity_x int64 `json:"velocity_x" redis:"velocity_x"`
-	Velocity_y int64 `json:"velocity_y" redis:"velocity_y"`
+	Uuid       string            `json:"uuid" redis:"uuid"`
+	Owner      string            `json:"owner" redis:"owner"`
+	Type       string            `json:"type" redis:"type"`
+	Global_X   int64             `json:"global_x" redis:"global_x"`
+	Global_Y   int64             `json:"global_y" redis:"global_y"`
+	Velocity_x int64             `json:"velocity_x" redis:"velocity_x"`
+	Velocity_y int64             `json:"velocity_y" redis:"velocity_y"`
 	Attributes map[string]string `json:"attributes" redis:"attributes"`
 }
-
 
 // These will be in attributes for now
 // Decay int64 `json:"decay" redis:"decay"`
